4a2245992a56a6bdefc2d028812bdabfbcbd20da
mmilton <murray.l.milton@outlook.com>
2023-01-28 06:30:18 +0100

    added the inheritance example package folder

---
+++ JavaMasterCourse/src/inheritanceExample/PersonTester.java
@@ -0,0 +1,13 @@
+package inheritanceExample;
+
+public class PersonTester {
+    public static void main(String[]args){
+        Person sally =  new Person("Sally", "Phillips");
+        System.out.println(sally);
+        Student mike = new Student("Mike", "Thompson","12345");
+        System.out.println(mike);
+        StudentEmployee jeff = new StudentEmployee("Jeff", "Sam", "4567",
+                35.5,"#1523699");
+        System.out.println(jeff);
+    }
+}
