774ce26ec36f5f155f224326a354c0c832c6a104
mmilton <murray.l.milton@outlook.com>
2023-01-28 06:51:34 +0100

    added the polymorphism example package

---
+++ JavaMasterCourse/src/polymorphismExample/Boat.java
@@ -0,0 +1,8 @@
+package polymorphismExample;
+
+public class Boat extends Vehicle{
+    public Boat(){
+        super("Boat start", "Boat stop", "Boat speed",
+                "Boat direction");
+    }
+}
