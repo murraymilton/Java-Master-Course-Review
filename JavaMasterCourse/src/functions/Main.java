11ea51eb1d84a55d303d62fed067c37ba492e868
mmilton <murray.l.milton@outlook.com>
2023-01-30 11:54:26 +0100

    added a simple function example

---
+++ JavaMasterCourse/src/functions/Main.java
@@ -0,0 +1,17 @@
+package functions;
+
+public class Main {
+    public static void main(String[]args){
+        singSong();
+        singSong();
+        singSong();
+        singSong();
+        singSong();
+    }
+    public static void singSong(){
+        System.out.println("Don't blame it on the Sunshine");
+        System.out.println("Don't blame it on the Moonlight");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Blame it on the boogie\n");
+    }
+}
